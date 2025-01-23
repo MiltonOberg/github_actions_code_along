e9f71f3f404362f53202531629779e88425ffbb2
Milton Österberg <miltonwendel@outlook.com>
2025-01-23 10:11:50 +0100

    fixed formatting and lint

--- draw_grapth.py
+++ draw_grapth.py
@@ -3,10 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-
-
-
-df = pd.DataFrame({"x": np.array([1,2,3]), "y": np.array([2, 5 ,1])}    )
+df = pd.DataFrame({"x": np.array([1, 2, 3]), "y": np.array([2, 5, 1])})
 
 print(df)
 
@@ -14,9 +11,6 @@ plt.plot(df["x"], df["y"])
 
 
 plt.show()
-
-
-
 
 
 print("hejsan")
